04a826092cf15d072bd1b52d58e0dd37681aaca5
GROM-85 <richfron69@gmail.com>
2022-12-13 13:17:27 +0100

    final

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,28 +1,29 @@
 (() => {
-  const openMenuBtn = document.querySelector(".menu-btn");
-  const mobileMenu = document.querySelector(".menu-container")
-  const body = document.querySelector('body')
+  const openMenuBtn = document.querySelector(".js-open-menu");
+  const closeMenuBtn = document.querySelector(".js-close-menu");
+  const mobileMenu = document.querySelector(".menu-container");
+  const body = document.querySelector('body');
 
-    openMenuBtn.addEventListener("click", () => {
-      const isMenuOpened = openMenuBtn.getAttribute("aria-expanded") === "true" || false;
-      openMenuBtn.setAttribute("aria-expanded", !isMenuOpened);     
+  const toggleMenu = () => {
+    const isMenuOpened = openMenuBtn.getAttribute("aria-expanded") === "true" || false;
+    openMenuBtn.setAttribute("aria-expanded", !isMenuOpened);
       
-      mobileMenu.classList.toggle("is-open");
-      openMenuBtn.classList.toggle('is-open');
-      body.classList.toggle("no-scroll")      
+    mobileMenu.classList.toggle("is-open");
+    openMenuBtn.classList.toggle('is-open');
+    body.classList.toggle("no-scroll");
 
-      // const scrollLockMethod = !isMenuOpened ? 'disableBodyScroll' : 'ebnableBodyScroll';
-      // bodyScrollLock[scrollLockMethod](document.body);    
+    const scrollLockMethod = !isMenuOpened ? 'disableBodyScroll' : 'enableBodyScroll';
+    bodyScrollLock[scrollLockMethod](document.body);    
 
-  })
+  };
 
-  openMenuBtn.addEventListener('click', toggleMenu);
-  
-
+   openMenuBtn.addEventListener('click',toggleMenu);
+   closeMenuBtn.addEventListener('click',toggleMenu);
   
 
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
     if (!e.matches) return;
+    
     mobileMenu.classList.remove('is-open');
     openMenuBtn.classList.remove('is-open');
     body.classList.remove('no-scroll');
